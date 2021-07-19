b93986da7812defcfa7e531722edfbcb0bdac7d8
Garfonso <garfonso@mobo.info>
2021-07-19 16:59:59 +0200

    build?

--- build/lib/api.js
+++ build/lib/api.js
@@ -5,27 +5,203 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Api = void 0;
 const axios_1 = __importDefault(require("axios"));
+const url_1 = __importDefault(require("url"));
 // Headers:
 // Accept: application/json
 // Accept-Charset: utf-8
 // Accept-Encoding : gzip-deflate
 // Content-type: application/json
+//how to limit this?
+//maximum 5 requests per second
+//maximum 30 requests per hour
+//api says that measurements are taken every hour (?)
 const baseURL = 'https://interop.ondilo.com/';
 const refreshURL = baseURL + 'oauth2/token';
 const client_id = 'customer_api';
+const apiPrefix = baseURL + 'api/customer/v1/';
 class Api {
     constructor(options) {
-        this._accessToken = options.accessToken;
-        this._refreshToken = options.refreshToken;
+        this.accessToken = options.accessToken;
+        this.refreshToken = options.refreshToken;
+        this.log = options.log;
     }
-    async refreshToken() {
-        const response = await axios_1.default.post(refreshURL, {
-            refresh_token: this._refreshToken,
-            grant_type: 'refresh_token',
-            client_id
-        }, { headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            } });
+    async doRefreshToken() {
+        try {
+            const response = await axios_1.default.post(refreshURL, new url_1.default.URLSearchParams({
+                refresh_token: this.refreshToken,
+                grant_type: 'refresh_token',
+                client_id
+            }).toString(), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            if (response.status === 200) {
+                if (response.data && response.data.access_token) {
+                    this.accessToken = response.data.access_token;
+                    return true;
+                }
+                else {
+                    this.log.warn('Refresh failed: no token in response!!: ' + JSON.stringify(response.data));
+                }
+            }
+            else {
+                this.log.warn('Refresh failed: ' + response.status + ' - ' + response.statusText);
+            }
+        }
+        catch (e) {
+            if (axios_1.default.isAxiosError(e)) {
+                this.log.warn('Had network error during refresh ' + e);
+            }
+            else {
+                this.log.error('Unexpected error during refresh: ' + e);
+            }
+        }
+        return false;
+    }
+    async requestInfo(urlPart, method = 'get', triedRefresh = false) {
+        try {
+            this.log.debug('Using token: ' + this.accessToken);
+            const response = await axios_1.default.request({
+                url: apiPrefix + urlPart,
+                method,
+                responseType: method === 'get' ? 'json' : 'text',
+                /*transitional: {
+                    silentJSONParsing: true
+                },*/
+                headers: {
+                    Authorization: 'Bearer ' + this.accessToken,
+                    Accept: 'application/json',
+                    'Accept-Charset': 'utf-8',
+                    'Accept-Encoding': 'gzip, deflate'
+                }
+            });
+            if (typeof response.data === 'string') {
+                return JSON.parse(response.data);
+            }
+            return response.data;
+        }
+        catch (e) {
+            if (axios_1.default.isAxiosError(e)) {
+                const response = e.response || { status: 0, data: 'Unknown failure', headers: '' };
+                if (response.status === 401 && !triedRefresh) {
+                    this.log.debug('Old token: ' + this.accessToken);
+                    const refreshWorked = await this.doRefreshToken();
+                    if (refreshWorked) {
+                        this.log.debug('New token: ' + this.accessToken);
+                        return this.requestInfo(urlPart, method, true);
+                    }
+                }
+                else {
+                    this.log.warn(`API Error ${response.status} while getting ${urlPart}: ${response.data} - headers: ${response.headers}`);
+                }
+            }
+            else {
+                this.log.warn('Unexpected error getting ' + urlPart + ': ' + e.stack);
+            }
+        }
+        return null;
+    }
+    //===========================================================================================================
+    // ========== User stuff:
+    //===========================================================================================================
+    async getUser() {
+        const data = await this.requestInfo('user/info');
+        if (typeof data === 'string') {
+            return JSON.parse(data);
+        }
+        return data;
+    }
+    // will return:
+    //     "conductivity": "MICRO_SIEMENS_PER_CENTI_METER",
+    //     "hardness": "FRENCH_DEGREE",
+    //     "orp": "MILLI_VOLT",
+    //     "pressure": "HECTO_PASCAL",
+    //     "salt": "GRAM_PER_LITER",
+    //     "speed": "METER_PER_SECOND",
+    //     "temperature": "CELSIUS",
+    //     "volume": "CUBIC_METER"
+    async getUnits() {
+        const data = await this.requestInfo('user/units');
+        return data;
+    }
+    // Result:
+    // [
+    //     {
+    //         "id": 234,
+    //         "name": "John's Pool",
+    //         "type": "outdoor_inground_pool",
+    //         "volume": 15,
+    //         "disinfection": {
+    //             "primary": "chlorine",
+    //             "secondary": {
+    //                 "uv_sanitizer": true,
+    //                 "ozonator": false
+    //             }
+    //         },
+    //         "address": {
+    //             "street": "162 Avenue Robert Schuman",
+    //             "zipcode": "13760",
+    //             "city": "Saint-Cannat",
+    //             "country": "France",
+    //             "latitude": 43.612282,
+    //             "longitude": 5.3179397
+    //         },
+    //         "updated_at": "2019-11-27T23:00:21+0000"
+    //     },
+    //     {
+    //         ...
+    //     }
+    // ]
+    async getPools() {
+        const data = await this.requestInfo('pools');
+        return data;
+    }
+    async getDevice(id) {
+        const data = await this.requestInfo(`pools/${id}/device`);
+        return data;
+    }
+    async getConfiguration(id) {
+        const data = await this.requestInfo(`pools/${id}/configuration`);
+        return data;
+    }
+    //getShares...?
+    //===========================================================================================================
+    // ========== Measurements:
+    //===========================================================================================================
+    async getLastMeasures(id) {
+        const data = await this.requestInfo(`pools/${id}/lastmeasures?
+            types[]=temperature&
+            types[]=ph&
+            types[]=orp&
+            types[]=salt&
+            types[]=tds&
+            types[]=battery&
+            types[]=rssi`);
+        return data;
+    }
+    /**
+     * Get all measures of type for the last day / week / month
+     * @param id
+     * @param type
+     * @param period
+     */
+    async getMeasures(id, type, period) {
+        const data = await this.requestInfo(`pools/${id}/measure?
+            type=${type}&
+            period=${period}`);
+        return data;
+    }
+    //===========================================================================================================
+    // ========== Recommendations:
+    //===========================================================================================================
+    async getRecommendations(id) {
+        const data = await this.requestInfo(`pools/${id}/recommendations`);
+        return data;
+    }
+    async validateRecommendation(poolId, recommendationId) {
+        const response = await this.requestInfo(`pools/${poolId}/recommendations/${recommendationId}`, 'put');
+        return ['Done', 'done', 'DONE'].includes(response);
     }
 }
 exports.Api = Api;
--- build/main.js
+++ build/main.js
@@ -25,14 +25,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = __importStar(require("@iobroker/adapter-core"));
-// Load your modules here, e.g.:
-// import * as fs from "fs";
+const api_1 = require("./lib/api");
 class Ico extends utils.Adapter {
     constructor(options = {}) {
         super({
             ...options,
             name: 'ico',
         });
+        this.pollInterval = 0;
+        this.devices = [];
         this.on('ready', this.onReady.bind(this));
         // this.on('stateChange', this.onStateChange.bind(this));
         // this.on('objectChange', this.onObjectChange.bind(this));
@@ -49,10 +50,203 @@ class Ico extends utils.Adapter {
         this.log.info('Configured pollinterval: ' + this.config.pollinterval);
         this.log.info('refreshToken: ' + this.config.refreshToken);
         if (this.config.refreshToken) {
+            this.api = new api_1.Api({
+                accessToken: this.config.accessToken,
+                refreshToken: this.config.refreshToken,
+                log: this.log
+            });
+            await this.updateDevices();
+            if (this.config.pollinterval) {
+                this.pollInterval = Math.max(1, this.config.pollinterval) * 60 * 1000; //convert from minutes to milliseconds.
+                setTimeout(this.poll, this.pollInterval);
+            }
         }
         else {
             this.log.info('Not authorized, yet. Please see configuration.');
         }
+    }
+    async updateDevices() {
+        const devices = await this.getDevicesAsync();
+        const poolArray = await this.api.getPools();
+        for (const pool of poolArray) {
+            if (pool.id) {
+                const icoDevice = await this.api.getDevice(pool.id);
+                let found = false;
+                for (const device of devices) {
+                    const uuid = device._id.split('.').pop();
+                    if (uuid === icoDevice.uuid) {
+                        found = true;
+                        let needsUpdate = false;
+                        if (device.native.poolId !== pool.id) {
+                            needsUpdate = true;
+                            device.native.poolId = pool.id;
+                        }
+                        if (device.native.swVersion !== icoDevice.sw_version) {
+                            needsUpdate = true;
+                            device.native.swVersion = icoDevice.sw_version;
+                        }
+                        if (needsUpdate) {
+                            await this.setObjectAsync(device._id, device);
+                        }
+                        this.devices.push(device);
+                        //remove device from devices array:
+                        const index = devices.indexOf(device);
+                        if (index >= 0) {
+                            devices.splice(index, 1);
+                        }
+                        break;
+                    }
+                }
+                //create device from pool / device if necessary
+                if (!found) {
+                    const id = this.namespace + '.' + icoDevice.uuid;
+                    const deviceObj = {
+                        type: 'device',
+                        common: {
+                            name: pool.name
+                        },
+                        native: {
+                            poolId: pool.id,
+                            swVersion: icoDevice.sw_version,
+                            hasObjects: {}
+                        }
+                    };
+                    this.devices.push(deviceObj);
+                    await this.setObjectAsync(id, deviceObj);
+                }
+            }
+        }
+        //if we still have devices, those are not in the cloud anymore -> remove.
+        for (const device of devices) {
+            await this.deleteDeviceAsync(device._id); //does this work as intended??
+            /*const objectsToDelete = await this.getObjectListAsync({startkey: device._id + '.', endkey: device._id + '.\u9999'});
+            const promises = [];
+            for (const obj of objectsToDelete) {
+                promises.push(this.delObjectAsync(obj._id));
+            }*/
+        }
+    }
+    async createObjectForMeasurement(device, type) {
+        let role = 'state';
+        let unit = undefined;
+        switch (type) {
+            case 'temperature': {
+                role = 'value.temperature';
+                unit = '°C';
+                break;
+            }
+            case 'ph': {
+                role = 'value.ph';
+                break;
+            }
+            case 'orp': {
+                role = 'value.orp';
+                unit = 'mV';
+                break;
+            }
+            case 'salt': {
+                role = 'value.salt';
+                unit = 'mg/L';
+                break;
+            }
+            case 'tds': {
+                role = 'value.tds';
+                unit = 'ppm';
+                break;
+            }
+            case 'battery': {
+                role = 'value.battery';
+                unit = '%';
+                await this.setObjectNotExistsAsync(device._id + '.lowBat', {
+                    type: 'state',
+                    common: {
+                        name: 'Low battery warning',
+                        role: 'indicator.lowbat',
+                        read: true,
+                        write: false
+                    },
+                    native: {}
+                });
+                break;
+            }
+            case 'rssi': {
+                role = 'value.rssi';
+                unit = '%';
+                await this.setObjectNotExistsAsync(device._id + '.offline', {
+                    type: 'state',
+                    common: {
+                        name: 'Low wifi signal',
+                        role: 'indicator.maintenance.unreach',
+                        read: true,
+                        write: false
+                    },
+                    native: {}
+                });
+                break;
+            }
+        }
+        const id = device._id + '.' + type;
+        const stateObj = {
+            type: 'state',
+            common: {
+                name: type,
+                role: role,
+                read: true,
+                write: false,
+                unit: unit
+            },
+            native: {},
+        };
+        device.native.hasObjects[type] = true;
+        await this.setObjectNotExistsAsync(id, stateObj);
+    }
+    async updateMeasurementsOfDevice(device) {
+        const measures = await this.api.getLastMeasures(device.native.poolId);
+        const promises = [];
+        for (const measure of measures) {
+            if (measure.is_valid) {
+                if (!device.native.hasObjects[measure.data_type]) {
+                    await this.createObjectForMeasurement(device, measure.data_type);
+                }
+                const currState = await this.getStateAsync(device._id + '.' + measure.data_type);
+                if (!currState || currState.ts < measure.value_time.getTime()) {
+                    await this.setStateAsync(device._id + '.' + measure.data_type, {
+                        val: measure.value,
+                        ack: true,
+                        ts: measure.value_time.getTime()
+                    });
+                    if (measure.data_type === 'battery') {
+                        await this.setStateChangedAsync(device._id + '.lowBat', {
+                            val: measure.value < 20,
+                            ack: true,
+                            ts: measure.value_time.getTime()
+                        });
+                    }
+                    if (measure.data_type === 'rssi') {
+                        await this.setStateChangedAsync(device._id + '.offline', {
+                            val: measure.value < 5,
+                            ack: true,
+                            ts: measure.value_time.getTime()
+                        });
+                    }
+                }
+                else {
+                    this.log.debug(`Measurement for ${measure.data_type} was already recorded in state db.`);
+                }
+            }
+            else {
+                this.log.debug(`Did not read ${measure.data_type} for ${device.native.poolId} because ${measure.exclusion_reason}`);
+            }
+        }
+        await Promise.all(promises);
+    }
+    async poll() {
+        const promises = [];
+        for (const device of this.devices) {
+            promises.push(this.updateMeasurementsOfDevice(device));
+        }
+        await Promise.all(promises);
+        setTimeout(() => this.poll, this.pollInterval);
     }
     /**
      * Is called when adapter shuts down - callback has to be called under any circumstances!
